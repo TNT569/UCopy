06cb0c814ff07f5ec073ee4f96a8c73aec7b74a7
TNT569 <2637126081@qq.com>
2020-06-14 07:55:16 +0800

    更新

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UCopy
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string mode;
+            Console.Title = "U盘静默拷贝工具";
+            Console.WriteLine("选择模式");
+            Console.Write("你选择的模式:");
+            mode = Console.ReadLine().ToString();
+        }
+    }
+}
